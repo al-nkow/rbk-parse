ddb84e1df25d1d5d7133e8993c22f6ba1d41e1b9
Aleksey Kovalchuk <al.nkow@gmail.com>
2019-07-09 01:26:35 +0300

    production ready

--- index.php
+++ index.php
@@ -3,175 +3,104 @@
   require 'db.php';
   require 'init.php';
 
-
-
-
-
-
-
-$html = file_get_contents("https://www.rbc.ru/");
-
-phpQuery::newDocument($html);
-
-$links = pq("#js_news_feed_banner .js-news-feed-list")->find("a.js-news-feed-item");
-
-$full = array();
-
-foreach($links as $link){
-	$link = pq($link);
-  $linkUrl = $link->attr("href");
-
-  if (preg_match("/www\.rbc\.ru/", $linkUrl))
+  function full_trim($str)
   {
-    $url = $link->attr("href");
-    if(!empty($url)) $data_link = file_get_contents($url);
-
-    $document_с = phpQuery::newDocument($data_link);
-    $content = $document_с->find('.article__content');
-
-    $title = $content->find('.js-slide-title');
-    $overview = $content->find('.article__text__overview');
-    $image = $content->find('.article__main-image__image');
-    $body = $content->find('.article__text_free');
-
-    if ($title && $body)
-    {
-
-
-      $dataTitle = $title->text();
-      $dataOverview = $overview->text();
-      $dataImage = $image->attr("src");
-      $dataBody  = $body->text();
-
-      $full[] = array(
-        "title" => $dataTitle,
-        "overview" => $dataOverview,
-        "image" => $dataImage,
-        "body"  => $dataBody,
-        "link" => $url,
-      );
-
-
-
-
-
-
-
-
-
-
-
-
-      echo('<div style="border: 4px solid red;">');
-      echo('<div style="color: red; font-size: 20px;">$dataTitle</div>');
-      echo($dataTitle);
-      echo(' - <br/>');
-      echo('<div style="color: red; font-size: 20px;">$dataOverview</div>');
-      echo($dataOverview);
-      echo(' - <br/>');
-      echo('<div style="color: red; font-size: 20px;">$dataImage</div>');
-      echo($dataImage);
-      echo(' - <br/>');
-      echo('<div style="color: red; font-size: 20px;">$dataBody</div>');
-      echo($dataBody);
-      echo(' - <br/>');
-      echo('<div style="color: red; font-size: 20px;">$url</div>');
-      echo($url);
-      echo(' - <br/>');
-
-      $dataTitle    = $connection->real_escape_string($dataTitle);
-      $dataImage    = $connection->real_escape_string($dataImage);
-      $dataBody     = $connection->real_escape_string($dataBody);
-      $url          = $connection->real_escape_string($url);
-
-      $sql = "INSERT INTO `news` (`id`, `title`, `image`, `body`, `link`) VALUES
-      (NULL, '".$dataTitle."', '".$dataImage."', '".$dataBody."', '".$url."')";
-
-      $result = $connection->query($sql);
-
-      if ($result) {
-        echo $result;
-      } else {
-        echo 'ERROR';
-      }
-
-      echo('</div>');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
+    return trim(preg_replace('/\s{2,}/', ' ', $str));
   }
 
+  $html = file_get_contents("https://www.rbc.ru/");
 
+  phpQuery::newDocument($html);
+  $links = pq("#js_news_feed_banner .js-news-feed-list")->find("a.js-news-feed-item");
 
+  foreach($links as $link){
+    $link = pq($link);
+    $linkUrl = $link->attr("href");
 
+    if (preg_match("/www\.rbc\.ru/", $linkUrl))
+    {
+      $url = $link->attr("href");
+      if(!empty($url)) $data_link = file_get_contents($url);
 
+      $document_с = phpQuery::newDocument($data_link);
+      $content = $document_с->find('.article__content');
 
+      $title = $content->find('.js-slide-title');
+      $overview = $content->find('.article__text__overview');
+      $image = $content->find('.article__main-image__image');
+      $body = $content->find('.article__text_free');
 
+      if ($title && $body)
+      {
+        $dataTitle = $title->text();
+        $dataImage = $image->attr("src");
+        $dataBody  = $body->text();
 
-}
+        $dataTitle    = $connection->real_escape_string($dataTitle);
+        $dataImage    = $connection->real_escape_string($dataImage);
+        $dataBody     = $connection->real_escape_string($dataBody);
+        $url          = $connection->real_escape_string($url);
 
+        $sql = "INSERT INTO `news` (`id`, `title`, `image`, `body`, `link`) VALUES
+        (NULL, '".$dataTitle."', '".$dataImage."', '".$dataBody."', '".$url."')";
 
+        $result = $connection->query($sql);
 
+        if (!$result) {
+          echo 'ERROR';
+        }
+      }
+    }
+  }
+  phpQuery::unloadDocuments();
 
+  $getNewsQuery = 'SELECT * FROM `news`';
+  $news = $connection->query($getNewsQuery);
+  $records = $news->fetch_all(MYSQLI_ASSOC);
 
-
-
-phpQuery::unloadDocuments();
+  if (!$news) {
+    echo 'ERROR';
+  }
 ?>
 
-
-<!--ul> 
-  <?php foreach($full as $value): ?> 
-    <li> 
-      <h3>
-        <?php echo($value['title']); ?>
-      </h3> 
-      <div>
-        <a href="./news.php?id=5" target="_blank">
-        111 сылка на новость
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+  <link href="./style.css" rel="stylesheet" type="text/css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="wrap">
+    <div class="head">
+      Новости
+    </div>
+    <div class="content">
+      <?php foreach($records as $value): ?>
+      <div class="preview">
+        <div class="preview__title">
+          <?php echo($value['TITLE']); ?>
+        </div>
+        <div class="preview__body">
+          <?php echo(mb_substr(full_trim($value['BODY']), 0, 200)); ?>...
+        </div>
+        <a href="./news.php?id=<?php echo($value['ID']); ?>" class="more">
+          Подробнее
         </a>
       </div>
-      <div>
-        <a href="<?php echo($value['link']); ?>" target="_blank">
-        оригинал статьи
-        </a>
-      </div>
-      <img src="<?php echo($value['image']); ?>"> 
-      <p>
-        <?php echo($value['overview']); ?>
-      </p>
-      <div>
-        <?php echo($value['body']); ?>
-      </div> 
-    </li> 
-  <?php endforeach; ?>
- </ul-->
+      <?php endforeach; ?>
+    </div>
+  </div>
+</body>
+</html>
 
 
 
 
 
 
-<script>
-  console.log('>>>>>>');
-</script>
 
 
--- news.php
+++ news.php
@@ -1,9 +1,49 @@
 <?php
   require 'db.php';
 
+  $i = $_GET['id'];
+  $sql = "SELECT * FROM `news` WHERE id='$i'";
+  $result = $connection->query($sql);
 
+  $records = $result->fetch_all(MYSQLI_ASSOC);
 
-
-
-
+  if (!$result) {
+    echo 'ERROR';
+  }
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+  <link href="./style.css" rel="stylesheet" type="text/css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="wrap">
+    <div class="head">
+      <a href="/">Новости</a>
+    </div>
+    <div class="content">
+      <?php foreach($records as $value): ?> 
+        <div class="title">
+          <?php echo($value['TITLE']); ?>
+        </div>
+        <img class="post-img" src="<?php echo($value['IMAGE']); ?>"> 
+        <div class="post-body">
+          <?php echo($value['BODY']); ?>
+        </div>
+      <?php endforeach; ?>
+      <a class="source" href="<?php echo($value['LINK']); ?>" target="_blank">
+        Источник
+      </a>
+    </div>
+  </div>
+</body>
+</html>
+
+
+
